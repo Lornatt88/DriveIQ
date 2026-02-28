4e2dd191bd29c22cec30102d92e8a0ce3b72d503
ziyanhashim <ziyanhashim2103@gmail.com>
2026-02-28 23:07:55 +0400

    fix(config): update LAN_IP address and simplify API_BASE_URL export

--- frontend/lib/config.ts
+++ frontend/lib/config.ts
@@ -7,15 +7,16 @@ const USE_NGROK = true;
 const NGROK_BASE = "https://steely-exultant-piper.ngrok-free.dev";
 
 // Home Wi-Fi LAN IP (only if USE_NGROK = false)
-const LAN_IP = "192.168.0.6";
+const LAN_IP = "192.168.31.166";
 const PORT = 8000;
 
 // Android emulator only (not Expo Go on phone)
 const ANDROID_EMULATOR_BASE = "http://10.0.2.2:8000";
 
 // ====== EXPORT ======
-export const API_BASE_URL = USE_NGROK
-  ? NGROK_BASE
-  : Platform.OS === "android"
-  ? ANDROID_EMULATOR_BASE
-  : `http://${LAN_IP}:${PORT}`;
+export const API_BASE_URL = `http://localhost:8000`;
+// export const API_BASE_URL = USE_NGROK
+//   ? NGROK_BASE
+//   : Platform.OS === "android"
+//   ? ANDROID_EMULATOR_BASE
+//   : `http://${LAN_IP}:${PORT}`;
